c4a573a02323e078c201749490ff9ae29c6f43b7
Kunal <futuremaker1001@gmail.com>
2023-06-07 08:36:58 +0530

    Connecting to firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDNsxdzg-c483TgMvwMMRVAE8Vy43jZbps",
+    authDomain: "angular-firebase-a49df.firebaseapp.com",
+    projectId: "angular-firebase-a49df",
+    storageBucket: "angular-firebase-a49df.appspot.com",
+    messagingSenderId: "372707656901",
+    appId: "1:372707656901:web:a6b8f24356ca0954f2dcac"
+  },
+};
